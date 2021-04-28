ac572eb67908e80753b7dc64c807e758fe0215dd
Manav Kushwaha <64668859+manavkush@users.noreply.github.com>
2021-04-28 19:53:09 +0530

    Added the header Bar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBeZ4pYo6Q6HWUTrzAzAbiGj6mBwuetmM4",
+    authDomain: "e-commerce-app-73c4b.firebaseapp.com",
+    projectId: "e-commerce-app-73c4b",
+    storageBucket: "e-commerce-app-73c4b.appspot.com",
+    messagingSenderId: "530097590512",
+    appId: "1:530097590512:web:7aa36489613da172357235",
+    measurementId: "G-LPY0WNSGTN"
+  };
